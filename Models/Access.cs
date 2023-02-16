cfe36a927b16674d80af412136a3348a465890db
Zwelisha Langa <zwelisha@bbd.co.za>
2023-02-16 05:14:23 +0200

    Added code to room and access classes

--- Models/Access.cs
+++ Models/Access.cs
@@ -2,5 +2,20 @@
 {
     public class Access
     {
+        public int accessID = 0;
+        public string accessName = "";
+
+        public int AccessID
+        {
+            get { return accessID; }
+            set { accessID = value; }
+        }
+        public string AccessName
+        {
+            get { return accessName; }
+            set { accessName = value; }
+        }
+
+        public Access() { }
     }
 }
--- Models/Room.cs
+++ Models/Room.cs
@@ -2,5 +2,44 @@
 {
     public class Room
     {
+        public int roomID = 0;
+        public int roomNumber = 0;
+        public int tenantID = 0;
+        public bool hasRoomAccess = false;
+        public DateOnly fromDate = new();
+        public DateOnly toDate = new();
+
+        public int RoomID
+        {
+            get { return roomID; }
+            set { roomID = value; }
+        }
+        public int RoomNumber
+        {
+            get { return roomNumber; }
+            set { roomNumber = value; }
+        }
+        public int TenantID
+        {
+            get { return tenantID; }
+            set { tenantID = value; }
+        }
+        public bool HasRoomAccess
+        {
+            get { return hasRoomAccess; }
+            set { hasRoomAccess = value; }
+        }
+        public DateOnly FromDate
+        {
+            get { return fromDate; }
+            set { fromDate = value; }
+        }
+        public DateOnly ToDate
+        {
+            get { return toDate; }
+            set { toDate = value; }
+        }
+
+        public Room() { }
     }
 }
